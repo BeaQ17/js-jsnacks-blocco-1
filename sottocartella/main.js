b6de1a73c5979d454eed8b14deca92a71263c6aa
BeaQ17 <beaq235@gmail.com>
2021-05-10 11:49:12 +0200

    1b

--- sottocartella/main.js
+++ sottocartella/main.js
@@ -7,5 +7,13 @@
 var palla_obj = {
     nome: "palla",
     peso: 10
-}
+};
 
+console.log(palla_obj);
+
+//1b
+//Attraverso un prompt dare possibilità all'utente di modificare il peso della palla
+var new_w = Number(prompt("Cambia il peso della palla:"));
+
+palla_obj.peso = new_w;
+console.log(palla_obj.peso);
